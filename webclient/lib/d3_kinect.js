f61ef62926bf0b392cad5d1db00e24e49e37de2c
chorsley <cmrhorsley@gmail.com>
2012-04-27 23:36:42 +1000

    added threshold values for glitchy co-ords - not great. Cycles colours when you wave

--- webclient/lib/d3_kinect.js
+++ webclient/lib/d3_kinect.js
@@ -7,8 +7,18 @@ var kinect_d3 = function(){
     that.jitter_buffer = 0.25;
 
     that.last_coords = [-1, -1, -1];
+    that.threshes = [1000, 1000, 3000];
+    that.colors = ["red", "blue", "orange", "pink", "black"];
 
-    that.depth_range = d3.scale.linear()
+    that.x_range = d3.scale.linear()
+                          .domain([500, -500])
+                          .range([0, that.canh]);
+    
+    that.y_range = d3.scale.linear()
+                          .domain([500, -500])
+                          .range([0, that.canw]);
+    
+    that.z_range = d3.scale.linear()
                           .domain([0, 7000])
                           .range([2, that.canw]);
 
@@ -23,6 +33,7 @@ var kinect_d3 = function(){
              .attr("width", 500)
              .attr("height", 500);
         that.draw();
+        that.wave_count = 0;
     }
 
     that.draw = function(){
@@ -36,14 +47,33 @@ var kinect_d3 = function(){
 
     that.grow_circle = function(coords){
 
-        var depth = coords[2];
+        var x = coords[0];
+        var y = coords[1];
+        var z = coords[2];
+
+        for (var i = 0; i < threshes.length; i++){
+            //console.log(coords[i], threshes[i]);
+            if (Math.abs(coords[i]) > threshes[i]){
+                return;
+            }
+        }
 
         var circ = d3.select(".circ")
-                         .attr("r", that.depth_range(coords[2]))
-                         .attr("fill", that.color_range(coords[2]))
+                         .attr("r", that.z_range(z))
+                         //.attr("fill", that.color_range(z))
+                         .attr("cx", that.x_range(x))
+                         .attr("cy", that.y_range(y))
     }
-    _.throttle(that.grow_circle)
-    //eve.on("kinect.wave", that.grow_circle)
+
+    that.next_color = function(){
+        that.wave_count = that.wave_count + 1;
+        d3.select(".circ")
+          .transition()
+          .attr("fill", that.colors[that.wave_count % (that.colors.length - 1)]);
+    }
+
+    //_.throttle(that.grow_circle)
+    eve.on("kinect.wave", that.next_color)
     eve.on("kinect.raisehand", that.grow_circle)
 
     return that;
