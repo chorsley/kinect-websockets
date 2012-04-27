9c74e6060cd58d7113d80ea36a46c4a7b938cf7f
chorsley <cmrhorsley@gmail.com>
2012-04-27 21:17:09 +1000

    Initial commit

---
+++ server/kinect.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This file is part of PyOpenNI.
+#
+# PyOpenNI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyOpenNI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with PyOpenNI.  If not, see <http://www.gnu.org/licenses/>.
+#
+# PyOpenNI is Copyright (C) 2011, Xavier Mendez (jmendeth).
+# OpenNI Python Wrapper (ONIPY) is Copyright (C) 2011, Gabriele Nataneli (gamix).
+#
+# ***** END GPL LICENSE BLOCK *****
+
+"""
+A nice and complete example of OpenNI:
+
+it starts tracking some gestures and alerts you
+when they're recognized.
+"""
+
+from openni import *
+import redis
+import json
+
+try:
+    red = redis.Redis(host='localhost', db=1)
+    chan_name = "kinect"
+
+    print "Initializing..."
+    ctx = Context()
+    ctx.init()
+    gest = GestureGenerator()
+    gest.create(ctx)
+
+    # Add some gestures to look for
+    print "Registering listeners..."
+    gest.add_gesture("Wave")
+    gest.add_gesture("Click")
+    gest.add_gesture("RaiseHand")
+    gest.add_gesture("MovingHand")
+
+    # Register callbacks
+    def gesture_detected(src, gesture, id, endPoint):
+        print "Detected gesture:", gesture
+        print "    Id: %s" % id
+        print "    Endpoint: %s" % endPoint
+        print "    Src: %s" % src
+        data = json.dumps({"coord": id, "gesture": gesture})
+        print data
+        red.publish(chan_name, data)
+
+    def gesture_progress(src, gesture, point, progress): pass
+
+    gest.register_gesture_cb(gesture_detected, gesture_progress)
+
+    # Start generating
+    print "Ready! Starting to detect gestures."
+    ctx.start_generating_all()
+
+    # Main loop:
+    # process every frame until the user interrupts
+    try:
+        print "Press Control-C to quit.\n"
+        while True: ctx.wait_any_update_all()
+    except KeyboardInterrupt: print
+
+except OpenNIError, error:
+    # When an error occurs:
+    print "OpenNI raised an error:", error
---
+++ server/server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import threading
+import tornado.web
+import tornado.ioloop
+import tornado.websocket
+import tornado.httpserver
+import redis
+
+LISTENERS = []
+CHAN = "kinect"
+
+def redis_listener():
+    print "Redis thread started"
+    r = redis.Redis(host='localhost', db=1)
+    chan = r.pubsub()
+    chan.subscribe([CHAN])
+    for message in chan.listen():
+        #print "Got event %s" % message['data']
+        for element in LISTENERS:
+            element.write_message(unicode(message['data']))
+
+class RealtimeHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print "Socket connected"
+        LISTENERS.append(self)
+
+    def on_message(self, message):
+        pass
+
+    def on_close(self):
+        LISTENERS.remove(self)
+
+
+settings = {
+        'auto_reload': True,
+        }
+
+application = tornado.web.Application([
+    (r'/', RealtimeHandler),
+    ], **settings)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=redis_listener).start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
